44b302ca9db5ca30f5bb6c53764425884584f742
oLucasNascimento <lucasf.ferreira@bandtec.com.br>
2025-02-27 11:06:56 -0300

    Initial commit to creation of microservice of Responsible

---
+++ src/test/java/com/sportsfinance/responsible/SportsFinanceMsResponsibleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sportsfinance.responsible;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsFinanceMsResponsibleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
